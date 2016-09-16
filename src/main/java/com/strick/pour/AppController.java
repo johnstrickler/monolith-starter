11ab00d365fd651dc3d7e150b6fcb216f62ba380
John Strickler <john.strickler@area101.com>
2016-09-16 16:16:01 -0400

    initial

---
+++ src/main/java/com/strick/pour/AppController.java
@@ -0,0 +1,15 @@
+package com.strick.pour;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+
+}
---
+++ src/main/webapp/app/index.ftl
@@ -0,0 +1,10 @@
+[#ftl]
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>TypeScript Testing</title>
+</head>
+<body>
+    Test fcdsfa as fdsf
+</body>
+</html>
